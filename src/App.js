5bf44f93008f162e6936779598a3c378ca86401a
levanthi <aotamez1@gmail.com>
2021-11-22 15:38:31 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -42,7 +42,6 @@ function App()
         
         return s
     }
-    console.log(data)
     return(
         <>
             <input value={data} onChange={()=>setData(data)} className={clsx(styles.display)}/>
